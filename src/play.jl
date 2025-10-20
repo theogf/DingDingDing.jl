1dad5237bc1d21fee2311c2f7d59ba0bd37c95b5
Henrik Wolf <h.wolf@amolf.nl>
2025-10-20 16:21:00 +0200

    use non-deprecated way of calling ffplay

--- src/play.jl
+++ src/play.jl
@@ -1,18 +1,17 @@
 function play(file::AbstractString; loop = false)
     @assert isfile(file)
-    ffplay() do exe
-        args = ["-nodisp", "-v", "0", "-autoexit"]
-        if loop
-            append!(args, ["-loop", "9999"])
-        end
-        @debug "Running play command $(Cmd(vcat(exe, args, file)))"
-        run(
-            addenv(
-                Cmd(vcat(exe, args, file)),
-                "ALSA_CONFIG_PATH" => "/usr/share/alsa/alsa.conf",
-                "ALSA_PLUGIN_DIR" => "/usr/lib/alsa-lib/",
-            );
-            wait = false,
-        )
+    args = ["-nodisp", "-v", "0", "-autoexit"]
+    if loop
+        append!(args, ["-loop", "9999"])
     end
+    @debug "Running play command $(Cmd(vcat(exe, args, file)))"
+
+    run(
+        addenv(
+            `$(ffplay()) $args $file`,
+            "ALSA_CONFIG_PATH" => "/usr/share/alsa/alsa.conf",
+            "ALSA_PLUGIN_DIR" => "/usr/lib/alsa-lib/",
+        ),
+        wait = false,
+    )
 end
